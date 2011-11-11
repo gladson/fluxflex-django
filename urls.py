51bd5279fbc5978e6c8c1772d8acaf4b46c5905a
root <gladson@agronomo.eng.br>
2011-11-11 00:48:54 -0200

    Alteracao urls.py

--- urls.py
+++ urls.py
@@ -1,17 +1,17 @@
-﻿from django.conf.urls.defaults import *
+﻿from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
+    # Examples:
+    # url(r'^$', 'digitalcom.views.home', name='home'),
+    # url(r'^digitalcom/', include('digitalcom.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #(r'^admin/(.*)', admin.site.root),
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
